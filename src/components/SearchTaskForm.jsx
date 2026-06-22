ec483f5f6f0d686b62b36f831cfa738978f04a0d
Daniil Mirovshchikov <mirovshch@gmail.com>
2026-06-22 14:05:26 +0300

    Adding event listeners

--- src/components/SearchTaskForm.jsx
+++ src/components/SearchTaskForm.jsx
@@ -1,23 +1,26 @@
-const SearchTaskForm = () => {
-    return (
-        <form className="todo__form">
-        <div className="todo__field field">
-          <label
-            className="field__label"
-            htmlFor="search-task"
-          >
-            Search task
-          </label>
-          <input
-            className="field__input"
-            id="search-task"
-            placeholder=" "
-            autoComplete="off"
-            type="search"
-          />
-        </div>
-      </form>
-    )
+import Field from "./Field"
+
+const SearchTaskForm = (props) => {
+  
+  const {
+    onSearchInput,
+  } = props
+
+  return (
+    <form
+      className="todo__form"
+      onSubmit={event.preventDefault()}
+    >
+      <Field 
+        className="todo__field"
+        label="Search task"
+        id="search-task"
+        type="search"
+        onInput={(event) =>
+          onSearchInput(event.target.value)}
+      />
+    </form>
+  )
 }
 
 export default SearchTaskForm
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,51 @@ import TodoInfo from "./TodoInfo"
 import TodoList from "./TodoList"
 
 const Todo = () => {
+
+    const tasks = [
+        {id: 'task-1', title: 'Купить молоко', isDone: false},
+        {id: 'task-2', title: 'Погладить кота', isDone: true},
+    ]
+    
+    const deleteAllTasks = () => {
+        console.log('Удаляем все задачи!')
+    }
+
+    const deleteTask = (taskID) => {
+        console.log(`Удалям задачу с id ${taskID}`)
+    }
+
+    const toggleTaskComplete = (taskID, isDone) => {
+        console.log(`Задача ${taskID} ${isDone ? 'выполнена' : 'не выполнена'}`)
+    }
+    
+    const filterTasks = (query) => {
+        console.log(`Поиск: ${query}`)
+    }
+
+    const addTask = () => {
+        console.log('Задача добавлена!')
+    }
+
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
-            <AddTaskForm />
-            <SearchTaskForm />
-            <TodoInfo />
-            <TodoList />
+            <AddTaskForm
+                addTask={addTask}
+            />
+            <SearchTaskForm
+                onSearchInput={filterTasks}
+            />
+            <TodoInfo 
+                total={tasks.length}
+                done={tasks.filter(({isDone}) => isDone).length} 
+                onDeleteAllButtonClick={deleteAllTasks}
+            />
+            <TodoList
+                tasks={tasks}
+                onDeleteTaskButtonClick={deleteTask}
+                onTaskCompleteChange={toggleTaskComplete}
+            />
         </div>
     )
 }
